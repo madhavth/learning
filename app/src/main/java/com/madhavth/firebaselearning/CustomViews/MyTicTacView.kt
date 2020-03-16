50b1f9b3cb3bd6e7c86f257577c3fbd01d2b951d
madhavth <powermt2012@gmail.com>
2020-03-16 22:04:30 +0545

    working winning paint

--- app/src/main/java/com/madhavth/firebaselearning/CustomViews/MyTicTacView.kt
+++ app/src/main/java/com/madhavth/firebaselearning/CustomViews/MyTicTacView.kt
@@ -8,17 +8,21 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 enum class TURN{
-    PLAYER2, PLAYER
+    PLAYER2, PLAYER1
 }
 
 fun TURN.change(): TURN {
-    return if(this == TURN.PLAYER)
+    return if(this == TURN.PLAYER1)
         TURN.PLAYER2
     else
-        TURN.PLAYER
+        TURN.PLAYER1
 }
 
 
@@ -28,18 +32,24 @@ class MyTicTacView(context: Context, attributeSet: AttributeSet): View(context, 
     private val paintBG = Paint()
     private var ticTacLayout= TicTacLayout(0,0,0,0)
     private var paintText = Paint()
-    private var turn  =TURN.PLAYER
+    private var paintWin = Paint()
+    private var turn  =TURN.PLAYER1
     private var count = 0
     private var winner: TURN? = null
     private var winCondition = WinCondition(0,0,0,0,0,0,0,0)
 
+    private var gameStopped = false
+
+    //could be better like game board List<List<>> i.e. [["x","o","x"],["","",""],["","",""]]
+    // but  too far too late to realize this easier way andd too lazy to edit everyyyyy thing
     private var mapDrawInfos = HashMap<Points,DrawInfo>()
 
+    private var tempLine = Points(0f,0f)
+
     private val Int.dp: Float
-        get()
-    {
+        get() {
         return this * resources.displayMetrics.density
-    }
+        }
 
 
     init{
@@ -58,6 +68,12 @@ class MyTicTacView(context: Context, attributeSet: AttributeSet): View(context, 
         paintBG.apply {
             isAntiAlias = true
             strokeWidth = (right -left).toFloat()
+        }
+
+        paintWin.apply {
+            isAntiAlias= true
+            strokeWidth = 8f
+            color = Color.WHITE
         }
 
         //init test for now
@@ -94,7 +110,7 @@ class MyTicTacView(context: Context, attributeSet: AttributeSet): View(context, 
             Timber.d("mapDrawInfo mapping $it")
             if(it.value.points.x !=0f && it.value.points.y != 0f)
             {
-                if(it.value.player == TURN.PLAYER)
+                if(it.value.player == TURN.PLAYER1)
                 {
                     paintText.color = Color.YELLOW
                 }
@@ -106,123 +122,146 @@ class MyTicTacView(context: Context, attributeSet: AttributeSet): View(context, 
 
                 Timber.d("map Index  ${it.key}=> Points ${it.value.points}, Player = ${it.value.player}")
                 canvas?.drawText(
-                    if(it.value.player == TURN.PLAYER)
+                    if(it.value.player == TURN.PLAYER1)
                         "O" else "X", it.value.points.x-width/12, it.value.points.y+height/12, paintText)
             }
         }
 
         //todo: optimize this long line of code
 
+        var possibleSolution = mutableListOf<MutableMap<Points,DrawInfo>>()
+
         val diagonal1 = mapDrawInfos.filter {
-            it.key.x == it.key.y && it.value.player == TURN.PLAYER
+            it.key.x == it.key.y && it.value.player == TURN.PLAYER1
         }
 
         val diagonal2 = mapDrawInfos.filter {
             it.key.x == it.key.y && it.value.player == TURN.PLAYER2
         }
 
-        val diagonal11 = mapDrawInfos.filter {
-            it.key.x + it.key.y == 2f  && it.value.player == TURN.PLAYER
+        val diagonal3 = mapDrawInfos.filter {
+            it.key.x + it.key.y == 2f && it.value.player == TURN.PLAYER1
         }
 
-        val diagonal22 = mapDrawInfos.filter {
+        val diagonal4 = mapDrawInfos.filter {
             it.key.x + it.key.y == 2f && it.value.player == TURN.PLAYER2
         }
 
+        possibleSolution.add(diagonal1.toMutableMap())
+        possibleSolution.add(diagonal2.toMutableMap())
+        possibleSolution.add(diagonal3.toMutableMap())
+        possibleSolution.add(diagonal4.toMutableMap())
 
-        if(diagonal1.size == 3 || diagonal2.size == 3 || diagonal11.size == 3 || diagonal22.size == 3)
+        for(i in 0..2) {
+
+            val rowCheck1 = mapDrawInfos.filter {
+                it.key.x == i.toFloat() && it.value.player == TURN.PLAYER1
+            }
+
+            val rowCheck2 = mapDrawInfos.filter {
+                it.key.x == i.toFloat() && it.value.player == TURN.PLAYER2
+            }
+
+            val colCheck1 = mapDrawInfos.filter {
+                it.key.y == i.toFloat() && it.value.player == TURN.PLAYER1
+            }
+
+            val colCheck2 = mapDrawInfos.filter {
+                it.key.y == i.toFloat() && it.value.player == TURN.PLAYER2
+            }
+
+            possibleSolution.add(rowCheck1.toMutableMap())
+            possibleSolution.add(rowCheck2.toMutableMap())
+            possibleSolution.add(colCheck1.toMutableMap())
+            possibleSolution.add(colCheck2.toMutableMap())
+        }
+
+
+        var playerWinner=  mutableMapOf<Points, DrawInfo>()
+        try{
+            playerWinner = possibleSolution.filter {
+                it.size == 3
+            }.first()
+        }
+        catch (e: Exception)
         {
-            winner = turn.change()
+
         }
 
 
-        val checkRow0 =  mapDrawInfos.filter {
-            it.key.x == 0f && it.value.player == TURN.PLAYER
-        }
-        val checkRow1 =  mapDrawInfos.filter {
-            it.key.x == 1f && it.value.player == TURN.PLAYER
-        }
-        val checkRow2 =  mapDrawInfos.filter {
-            it.key.x == 2f && it.value.player == TURN.PLAYER
-        }
-
-
-        val checkColumn1 =  mapDrawInfos.filter {
-            it.key.y == 0f && it.value.player == TURN.PLAYER
-        }
-        val checkColumn2 =  mapDrawInfos.filter {
-            it.key.y == 1f && it.value.player == TURN.PLAYER
-        }
-        val checkColumn3 =  mapDrawInfos.filter {
-            it.key.y == 2f && it.value.player == TURN.PLAYER
-        }
-
-        //player2
-        val checkRow00 =  mapDrawInfos.filter {
-            it.key.x == 0f && it.value.player == TURN.PLAYER2
-        }
-        val checkRow11 =  mapDrawInfos.filter {
-            it.key.x == 1f && it.value.player == TURN.PLAYER2
-        }
-        val checkRow22 =  mapDrawInfos.filter {
-            it.key.x == 2f && it.value.player == TURN.PLAYER2
-        }
-
-
-        val checkColumn11 =  mapDrawInfos.filter {
-            it.key.y == 0f && it.value.player == TURN.PLAYER2
-        }
-        val checkColumn22 =  mapDrawInfos.filter {
-            it.key.y == 1f && it.value.player == TURN.PLAYER2
-        }
-        val checkColumn33 =  mapDrawInfos.filter {
-            it.key.y == 2f && it.value.player == TURN.PLAYER2
-        }
-
-
-        //check for player
-        if(checkRow0.size == 3 || checkRow1.size == 3 || checkRow2.size == 3
-            || checkColumn1.size == 3 || checkColumn2.size == 3 || checkColumn3.size == 3)
+        if(playerWinner.isNotEmpty())
         {
-            winner = turn.change()
-        }
-
-        //check for player2
-        if(checkRow00.size == 3 || checkRow11.size == 3 || checkRow22.size == 3
-            || checkColumn11.size == 3 || checkColumn22.size == 3 || checkColumn33.size == 3)
-        {
-            winner = turn.change()
+            winner  = playerWinner.map {
+                it.value.player
+            }.first()
         }
 
         if (winner!=null)
         {
+            val fPoints = playerWinner.values
+            var pointsLine = mutableListOf<Points>()
+
+            fPoints.forEach {
+                val xF  = it.points.x
+                val xY = it.points.y
+
+                pointsLine.add(Points(xF,xY))
+            }
+
+            var minXY =pointsLine.minBy { it.x }!!
+            var maxXY = pointsLine.maxBy { it.x }!!
+
+            Timber.d("pointsLine are $minXY, $maxXY")
+            if(minXY.x == maxXY.x)
+            {
+                minXY = pointsLine.minBy { it.y }!!
+                maxXY = pointsLine.maxBy { it.y }!!
+            }
+
+            canvas?.drawLine(minXY.x,minXY.y, maxXY.x,maxXY.y,paintLine)
+
             Toast.makeText(context
             , "winner is $winner",
             Toast.LENGTH_SHORT).show()
-            winner =null
-            count = 0
-            mapDrawInfos.clear()
-            invalidate()
+
+            gameStopped =!gameStopped
+
+            CoroutineScope(Dispatchers.IO).launch {
+                gameOver()
+            }
         }
 
         else if (count == 9 && winner == null)
         {
             count = 0
             Toast.makeText(context
-            , "it's a drawwww!!! you are both terrible or equally good congrats",
-            Toast.LENGTH_SHORT).show()
-            invalidate()
+            , "it's a drawwww!!! you are both terrible or equally good or both congrats",
+            Toast.LENGTH_LONG).show()
+            gameStopped =!gameStopped
+
+            CoroutineScope(Dispatchers.IO).launch {
+                gameOver()
+            }
         }
-
-
     }
+
+    suspend fun gameOver()
+    {
+        delay(1500)
+        this@MyTicTacView.invalidate()
+        winner =null
+        count = 0
+        mapDrawInfos.clear()
+        gameStopped =!gameStopped
+    }
+
 
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         val temp =Points(0f,0f)
         val textXY = Points(0f,0f)
 
-        if(event?.action == MotionEvent.ACTION_DOWN)
+        if(event?.action == MotionEvent.ACTION_DOWN && !gameStopped)
         {
             //for getting key, value for map
 
@@ -271,7 +310,7 @@ class MyTicTacView(context: Context, attributeSet: AttributeSet): View(context, 
             {
                 mapDrawInfos[temp] = DrawInfo(textXY, turn)
                 count++
-                turn = if(turn == TURN.PLAYER) TURN.PLAYER2 else TURN.PLAYER
+                turn = if(turn == TURN.PLAYER1) TURN.PLAYER2 else TURN.PLAYER1
                 Timber.d("Testing left top bottom right are $left, $top, $bottom, $right")
                 Timber.d("Testing rawXY are ${event.rawX}, ${event.rawY}")
                 Timber.d("Testing textXY are $textXY")
